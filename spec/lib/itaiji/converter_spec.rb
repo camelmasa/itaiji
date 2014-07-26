f81c1ffadce88e43a58dc42f04d843417eee4fd8
camelmasa <camelmasa@gmail.com>
2014-07-26 19:18:43 +0900

    :lipstick:

--- spec/lib/itaiji/converter_spec.rb
+++ spec/lib/itaiji/converter_spec.rb
@@ -8,14 +8,14 @@ describe Itaiji::Converter do
   let(:converter) { Itaiji::Converter.new }
 
   describe '#convert_seijitai' do
-    it 'convert name from itaiji to seijitai' do
+    it 'converts name from itaiji to seijitai' do
       expect {
         converter.convert_seijitai(itaiji_text)
       }.to change{ itaiji_text }.to seijitai_text
     end
   end
   describe '#convert_itaiji' do
-    it 'convert name from seijitai to itaiji' do
+    it 'converts name from seijitai to itaiji' do
       expect {
         converter.convert_itaiji(seijitai_text)
       }.to change{ seijitai_text }.to itaiji_text
